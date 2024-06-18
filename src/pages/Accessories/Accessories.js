7455386a588f65069ad74e9bb89558c8b747592f
Kono1889 <agyemangkono>
2024-06-18 05:17:23 +0100

    accesories page implemented

--- src/pages/Accessories/Accessories.js
+++ src/pages/Accessories/Accessories.js
@@ -1,11 +1,112 @@
-import React from 'react'
+import React from "react";
+import Heading from "../../components/home/Products/Heading";
+import Product from "../../components/home/Products/Product";
+import {
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+} from "../../assets/images/index";
 
 const Accessories = () => {
   return (
-    <div>
-      Accessories
-    </div>
-  )
-}
+    <div className="w-full pb-16">
+      <Heading heading="Accessories" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-2">
+        <Product
+          _id="100001"
+          img={newArrOne}
+          productName="Round Table Clock"
+          price="44.00"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100002"
+          img={newArrTwo}
+          productName="Smart Watch"
+          price="250.00"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100003"
+          img={newArrThree}
+          productName="Cloth Basket"
+          price="80.00"
+          color="Mixed"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100004"
+          img={newArrFour}
+          productName="Funny toys for babies"
+          price="60.00"
+          color="Mixed"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100005"
+          img={newArrTwo}
+          productName="Funny toys for babies"
+          price="60.00"
+          color="Mixed"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
 
-export default Accessories
+<Product
+          _id="100001"
+          img={newArrOne}
+          productName="Round Table Clock"
+          price="44.00"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100002"
+          img={newArrTwo}
+          productName="Smart Watch"
+          price="250.00"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100003"
+          img={newArrThree}
+          productName="Cloth Basket"
+          price="80.00"
+          color="Mixed"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100004"
+          img={newArrFour}
+          productName="Funny toys for babies"
+          price="60.00"
+          color="Mixed"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="100005"
+          img={newArrTwo}
+          productName="Funny toys for babies"
+          price="60.00"
+          color="Mixed"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Accessories;
