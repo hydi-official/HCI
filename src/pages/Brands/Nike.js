e37ee8e7b4513ddd16f491cdcd8090c95b60154b
amnash <siakunor@st.ug.edu.gh>
2024-07-23 16:37:42 +0000

    nike page update

--- src/pages/Brands/Nike.js
+++ src/pages/Brands/Nike.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Heading from "../../components/home/Products/Heading";
 import Product from "../../components/home/Products/Product";
-import wig2 from "../../assets/images/donald-teel-kzyCcIgE2m4-unsplash.jpg";
-import wig3 from "../../assets/images/valerie-elash-XUT7V_md7sc-unsplash.jpg";
-import wig4 from "../../assets/images/vladimir-yelizarov-0XfjLwiI1sk-unsplash.jpg";
-import wig5 from "../../assets/images/joey-nicotra-erGcMFMaYGk-unsplash.jpg"
+import wig2 from "../../assets/images/shopbyorder/grailify-9Ch-39ZNDk0-unsplash.jpg";
+import wig3 from "../../assets/images/shopbyorder/karsten-winegeart-6Uz8LaZPZsE-unsplash.jpg";
+import wig4 from "../../assets/images/shopbyorder/mahaan-mehranii-kTegU2zb2tA-unsplash.jpg";
+import wig5 from "../../assets/images/shopbyorder/joecalih-kFtTXa5ZS1Q-unsplash.jpg"
 const Nike = () => {
   return (
     <div className="w-full pb-16">
@@ -13,38 +13,38 @@ const Nike = () => {
         <Product
           _id="100001"
           img={wig5}
-          productName="Round Table Clock"
+          productName="Nike Tops"
           price="44.00"
           color="Black"
           badge={true}
-          des="nnn ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          des="Nike tops blend performance and style effortlessly. Made with high-quality, breathable fabrics, they offer comfort and durability for any activity. With sleek designs and innovative features, these tops are perfect for workouts or casual wear, making them an essential addition to any active wardrobe."
         />
         <Product
           _id="100002"
           img={wig2}
-          productName="Smart Watch"
+          productName="Nike Joggers"
           price="250.00"
           color="Black"
           badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          des="Nike joggers provide the perfect combination of comfort and style. Made from premium, breathable materials, they offer a relaxed fit with an adjustable waistband. Ideal for workouts or casual wear, these joggers feature sleek designs and functional pockets, making them a versatile addition to any wardrobe."
         />
         <Product
           _id="100003"
           img={wig3}
-          productName="Cloth Basket"
+          productName="Nike shoes"
           price="80.00"
           color="Mixed"
           badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          des="Nike shoes offer unparalleled comfort, style, and performance. Crafted with innovative technology and premium materials, they provide excellent support and durability for various activities. With a wide range of designs and colors, these versatile shoes are perfect for both athletic pursuits and everyday wear, making them a must-have for any footwear collection."
         />
         <Product
           _id="100004"
           img={wig4}
-          productName="Funny toys for babies"
+          productName="Nike Jackets"
           price="60.00"
           color="Mixed"
           badge={false}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+          des="Nike jackets combine cutting-edge design with superior comfort and functionality. Crafted from high-quality materials, they provide excellent warmth and protection against the elements. Featuring sleek styles and innovative details, these jackets are perfect for both athletic and casual wear, making them an essential addition to any wardrobe."
         />
 
       </div>
